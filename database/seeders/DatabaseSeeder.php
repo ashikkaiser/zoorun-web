4d7ada7e73be298894fb6274182a3b94dca45f67
root <root@ip253.ip-15-235-136.net>
2022-09-19 12:21:09 -0400

    ss

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+
+            ParcelStatusSeeder::class,
+            AreaSedder::class,
+
+        ]);
 
         // \App\Models\User::create([
         //     'name' => 'Ashik',
